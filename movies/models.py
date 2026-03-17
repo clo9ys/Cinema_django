d0d78f33dd45222e65276963dd660b2546027a47
Stolpovskij <18737957+clo9ys@users.noreply.github.com>
2026-03-17 23:30:20 +0300

    feat: add Ganre and Movie models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Жанр")
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    AGE_LIMIT_CHOICES = [
+        (0, '0+'),
+        (6, '6+'),
+        (12, '12+'),
+        (16, '16+'),
+        (18, '18+')
+    ]
+
+    title = models.CharField(max_length=100, verbose_name="Название")
+    summary = models.TextField(verbose_name="Аннотация")
+    release_year = models.PositiveIntegerField(verbose_name="Год выхода")
+    duration_minutes = models.PositiveIntegerField(verbose_name="Продолжительность")
+
+    age_limit = models.PositiveIntegerField(choices=AGE_LIMIT_CHOICES, default=0, verbose_name="Возрастное ограничение")
+
+    genres = models.ManyToManyField(Genre, related_name="movies", verbose_name="Жанры")
+
+    def __str__(self):
+        return self.title
