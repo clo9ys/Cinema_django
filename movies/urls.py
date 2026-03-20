ffa039f23c585d27d409ba4c22abc12fdbf71a0b
David Shvarts <davidshvarts2805@gmail.com>
2026-03-20 20:53:51 +0300

    #7 Создание эндпоинтов /api/movies/ (список) и /api/movies/{id}/ (детали). Интеграция с роутерами

---
+++ movies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import MovieViewSet
+
+router = DefaultRouter()
+router.register('movies', MovieViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
