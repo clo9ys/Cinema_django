e79121021d775c8aeee0b6d7aaa53d252df1e08a
Stolpovskij <max2912071@gmail.com>
2026-04-22 00:15:47 +0300

    feat: add DTO layer #19

---
+++ users/dto.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class SubscriptionData:
+    user_id: int
+    sub_id: int
---
+++ users/services.py
@@ -0,0 +1,20 @@
+from django.db import transaction
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from .models import Subscription, UserProfile
+from config.exceptions import AlreadySubscribedError
+from .dto import SubscriptionData
+
+class SubscriptionService:
+    @transaction.atomic
+    def subscribe_user(self, data: SubscriptionData):
+        user = get_object_or_404(User, id=data.user_id)
+        plan = get_object_or_404(Subscription, id=data.sub_id)
+        profile, _ = UserProfile.objects.get_or_create(user=user)
+
+        if profile.subscription == plan:
+            raise AlreadySubscribedError(f"У пользователя уже есть подписка {plan.name}")
+
+        profile.subscription = plan
+        profile.save()
+        return profile
