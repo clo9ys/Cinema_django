eb3336969db94d1e8a10d2e726349a92f1c5f100
Stolpovskij <max2912071@gmail.com>
2026-04-22 20:32:03 +0300

    feat: add user viewset and router #20

--- users/views.py
+++ users/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .services.subscription_service import SubscriptionService
 from .models import WatchList
 from .serializers import WatchListSerializer
+from .dto import SubscriptionData
 
 
 class WatchListViewSet(viewsets.ModelViewSet):
@@ -23,3 +22,17 @@ class WatchListViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """при создании подставляет пользователя"""
         serializer.save(user=self.request.user)
+
+
+class UserViewSet(viewsets.ViewSet):
+    sub_service = SubscriptionService()
+
+    @action(detail=True, methods=['post'], url_path='subscribe')
+    def subscribe(self, request, pk=None):
+        dto = SubscriptionData(
+            user_id=int(pk),
+            sub_id=request.data.get("sub_id"),
+        )
+
+        self.sub_service.subscribe_user(dto)
+        return Response({"status": "success", "message": "Подписка обновлена"}, status=status.HTTP_200_OK)
