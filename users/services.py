1eaf8258013acee8241dc1e6c0d779431abdf520
Alexandr <mai.kopylovaa@gmail.com>
2026-04-22 17:22:43 +0300

    feat: add WatchListService class with user age check #23

--- users/services.py
+++ users/services.py
@@ -6,6 +6,7 @@ from config.exceptions import AlreadySubscribedError, AgeLimitError
 from .dto import SubscriptionData, WatchListData
 from movies.models import Movie
 
+
 class SubscriptionService:
     @transaction.atomic
     def subscribe_user(self, data: SubscriptionData):
@@ -19,3 +20,18 @@ class SubscriptionService:
         profile.subscription = plan
         profile.save()
         return profile
+
+
+class WatchListService:
+    @transaction.atomic
+    def add_movie_to_watchlist(self, data: WatchListData):
+        movie = get_object_or_404(Movie, id=data.movie_id)
+
+        user = get_object_or_404(User, id=data.user_id)
+        profile, _ = UserProfile.objects.get_or_create(user=user)
+        user_age = profile.age
+
+        if user_age <= movie.age_limit:
+            raise AgeLimitError
+
+        return WatchList.objects.create(user=user, movie=movie)
