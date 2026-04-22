aefaee7dc30263e7a976a5e70f91dd029dcb1b48
David Shvarts <davidshvarts2805@gmail.com>
2026-04-23 00:57:29 +0300

    #22 #21

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import MovieViewSet
+from .views import GenreViewSet, MovieViewSet
 
 router = DefaultRouter()
-router.register('movies', MovieViewSet)
+router.register('movies', MovieViewSet, basename='movie')
+router.register('genres', GenreViewSet, basename='genre')
 
 urlpatterns = [
     path('', include(router.urls)),
